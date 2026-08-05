24de1fe003e174a4ca150312c7bbc9069e5d02b9
Geza Jenes <47823190+Csutkas@users.noreply.github.com>
2026-08-05 16:41:05 +0200

    fix: required updates

--- lib/pages/home/home_screen.dart
+++ lib/pages/home/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/navigator/router.gr.dart';
 
 @RoutePage()
 class HomeScreen extends StatefulWidget {
